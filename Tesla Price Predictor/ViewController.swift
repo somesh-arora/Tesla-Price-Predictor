21e80161d952837c4d35a4607587cd73349d97f8
Somesh Arora <somesh.arora@mavs.uta.edu>
2019-05-20 20:20:43 -0500

    Initial Commit

---
+++ Tesla Price Predictor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tesla Price Predictor
+//
+//  Created by Somesh Arora on 5/20/19.
+//  Copyright © 2019 Somesh Arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
