7a5e393f0425320d7ae23b4c07e0db727caa6227
Somesh Arora <somesh.arora@mavs.uta.edu>
2019-05-21 21:14:52 -0500

    Telsa Price Predictor App utilizes Core ML to determine the value of your Tesla based on the model of your car, # of miles, upgrades, and condition.

--- Tesla Price Predictor/ViewController.swift
+++ Tesla Price Predictor/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let cars = Cars()
 
+    @IBOutlet weak var modelSegmentControl: UISegmentedControl!
+    @IBOutlet weak var upgradesSegmentControl: UISegmentedControl!
+    @IBOutlet weak var mileageSlider: UISlider!
+    @IBOutlet weak var conditionSegmentControl: UISegmentedControl!
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var mileageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calculateValue(self)
     }
-
-
+    @IBAction func calculateValue(_ sender: Any) {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 0
+        let formattedMileage = formatter.string(for: mileageSlider.value) ?? "0"
+        mileageLabel.text = "Mileage: (\(formattedMileage) miles)"
+        
+        if let prediction = try? cars.prediction(model: Double(modelSegmentControl.selectedSegmentIndex),
+                                                 premium: Double(upgradesSegmentControl.selectedSegmentIndex),
+                                                 mileage: Double(mileageSlider.value),
+                                                 condition: Double(conditionSegmentControl.selectedSegmentIndex)) {
+            let clampValuation = max(5000, prediction.price)
+            formatter.numberStyle = .currency
+            valueLabel.text = formatter.string(for: clampValuation)
+        } else {
+            valueLabel.text = "--"
+        }
+        
+    }
 }
 
